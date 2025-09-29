2076c4a07f915a25d4c075a16567879f4623cb78
Rohan Patel <patelrm2@wofford.edu>
2025-09-29 10:12:21 -0400

    got rid of extra COSC410-2025-AutoGrader file

--- COSC410-2025-AutoGrader/backend/app/core/settings.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-from pydantic import AnyUrl
-from typing import Optional
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./app.db"
-    CORS_ORIGINS: str = "http://localhost:5173"
-
-settings = Settings()
